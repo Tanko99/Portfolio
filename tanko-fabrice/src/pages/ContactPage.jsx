e90a0aae1040abfcb6fffa7e3845864b2fd79ad2
Tanko Ngala <tankofabrice4@gmail.com>
2025-11-12 11:22:51 +0100

    cotact page; border lines added and background color for button changed to be vissible in dark mode

--- tanko-fabrice/src/pages/ContactPage.jsx
+++ tanko-fabrice/src/pages/ContactPage.jsx
@@ -52,28 +52,28 @@ function ContactMe (){
                     <div className="flex flex-col">
                         <label htmlFor="subject" className="md:text-xl sm:text-lg text-base font-bold mb-2">Subject</label>
                         <Field id="subject" type="text" name="subject" 
-                          className="dark:bg-gray-900  border-2 border-accent dark:border-accent
+                          className="dark:bg-gray-900  border-2 border-primary dark:border-primary
                            px-4 py-2 w-60 md:w-96 focus:outline-none rounded-xl "/>
                         <ErrorMessage name="subject" component="p" className="md:text-xl text-base text-red-500 text-center" />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="name" className="md:text-xl sm:text-lg text-base font-bold  mb-2">Name</label>
                         <Field id="name" type="text" name="name" 
-                         className="dark:bg-gray-900  px-4 py-2 w-60 md:w-96 sm:w-64 mb-6 border-2 border-accent dark:border-accent
+                         className="dark:bg-gray-900  px-4 py-2 w-60 md:w-96 sm:w-64 mb-6 border-2 border-primary dark:border-primary
                            focus:outline-none rounded-xl " />
                         <ErrorMessage name="name" component="p" className="md:text-xl text-base text-red-600 text-center" />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="email" className="md:text-xl sm:text-lg text-base font-bold mb-2">Email</label>
                         <Field id="email" type="email" name="email" 
-                         className="px-4 py-2 w-60 md:w-96 sm:w-64 mb-6 border-2  border-accent dark:border-accent
+                         className="px-4 py-2 w-60 md:w-96 sm:w-64 mb-6 border-2  border-primary dark:border-primary
                           dark:bg-gray-900 focus:outline-none rounded-xl "  />
                         <ErrorMessage name="email" component="p" className="md:text-xl text-base text-red-500 text-center" />
                     </div>
                     <div className="flex flex-col " >
                         <label htmlFor="message" className="md:text-xl sm:text-lg text-base font-bold mb-2">Message</label>
                          <Field id="message" as="textarea" name="message"  maxLength="2000"
-                          className="p-10 w-60 md:w-96 sm:w-64 mb-6 border-2 border-accent dark:border-accent
+                          className="p-10 w-60 md:w-96 sm:w-64 mb-6 border-2 border-primary dark:border-primary
                          dark:bg-gray-900 focus:outline-none rounded-lg " />
                          <ErrorMessage name="message" component="p" className="md:text-xl text-base  text-red-500 text-center"  />
                     </div>
@@ -83,9 +83,9 @@ function ContactMe (){
                             {feedback.message}
                        </div>
                    )}
-                   <div className="mb-10">
-                    <button className="px-4 py-2 bg-accent dark:bg-accent text-gray-900 dark:text-gray-900  md:text-2xl smtext:xl text-lg w-60 md:w-80 sm:w-64 
-                    rounded-2xl font-bold flex gap-10" type="submit">
+                   <div>
+                    <button className="px-2 py-2 bg-primary dark:bg-primary text-gray-300 dark:text-gray-300  md:text-2xl smtext:xl text-lg w-60 md:w-80 sm:w-64 
+                    rounded-2xl font-bold flex  mb-16 gap-2 " type="submit">
                         
                         Send message <Send className="w-8 h-8"  />
                         </button>
