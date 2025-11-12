0df0be46b1ecdd648d4f60d2398f32b3b87b7b5f
Tanko Ngala <tankofabrice4@gmail.com>
2025-11-12 11:29:13 +0100

    project card; background color for buttonj changed so it can be visible in dark mode

--- tanko-fabrice/src/components/ProjectCard.jsx
+++ tanko-fabrice/src/components/ProjectCard.jsx
@@ -39,7 +39,7 @@ const ProjectCard = ({ thumbnail, title, description,
                     )}
                 </div>
                 <button onClick={toggleExpand} className=" 
-                px-2 py-2 bg-gray-900 text-gray-200 dark:bg-gray-200 dark:text-gray-900 rounded-xl px-4 py-2 flex my-4">
+                px-2 py-2 bg-gray-900 text-gray-200 dark:bg-primary dark:text-gray-300 rounded-xl px-4 py-2 flex my-4">
                     {isExpanded ? "Show less"  : 'Read more' } <ArrowBigDown  className="w-5 h-5" />
                 </button>
             </div>
